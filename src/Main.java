d9b3a048054f485fa539bec1f8548f7cf9daaf12
BuildTools <areyouagitated@gmail.com>
2017-07-13 05:59:31 -0400

    Second Commit

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Lbadv on 6/25/2017.
- */
-public class Main {
-    public static void main(String[] args) {
-        Support.Print();
-    }
-}
--- src/Support.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Lbadv on 6/25/2017.
- */
-public class Support {
-    public static void Print() {
-        System.out.println("Stuff");
-    }
-}
